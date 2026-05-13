6da3041ad24869380134a2f8d04be829e4bb38bf
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-14 00:29:17 +0530

    overriding behaviour of in built functions

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -7,7 +7,7 @@ multiline comment
 """
 print("Hello, ")
 name = input("What is your name? ")
-print("Hello")
+print("Hello ",end="") #end is used to change the end character of the print statement, by default it is a new line
 print(name)
 
 print("Hello, " + name) #concatenation
