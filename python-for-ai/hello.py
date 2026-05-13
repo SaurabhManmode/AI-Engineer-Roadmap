600d5412ddd8ffcef64abc7691cff798ea775453
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-14 00:09:29 +0530

    Concatenation and multiple line arguments

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,6 +1,14 @@
 
 #Ask user for there name variable example
+
+
+"""
+multiline comment
+"""
 print("Hello, ")
 name = input("What is your name? ")
 print("Hello")
 print(name)
+
+print("Hello, " + name) #concatenation
+print("Hello,",name) #multiple arguments
