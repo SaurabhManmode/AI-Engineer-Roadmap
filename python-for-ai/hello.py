aad71eae3ef7577257587f9013cc1d650871cf98
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-15 00:27:09 +0530

    strip and capitalize function

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -7,8 +7,14 @@ multiline comment
 """
 print("Hello, ")
 name = input("What is your name? ")
+
+name = name.strip() #strip is used to remove any leading or trailing whitespace from the input
+name = name.capitalize()
 print("Hello ",end="") #end is used to change the end character of the print statement, by default it is a new line
 print(name)
 
 print("Hello, " + name) #concatenation
 print("Hello,",name) #multiple arguments
+
+
+print(f"hello,{name}") #f string, it is a way to format strings in python, it is available in python 3.6 and above, it is more efficient than concatenation and multiple arguments, it is also more readable
