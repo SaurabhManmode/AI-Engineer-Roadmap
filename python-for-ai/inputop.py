51d5b4d04933424c37b5cccf79b884e9c2dcf281
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-06-02 01:36:11 +0530

    variable printing

---
+++ python-for-ai/inputop.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ") #input is used to get user input, it returns a string
+print("Hello, " + name) #concatenation, it is used to combine two
+
+s= "Brad"
+print(s)
+
+user="tatiyana"
+age=30
+city="NewYork"
+
+print(user,age,city)
