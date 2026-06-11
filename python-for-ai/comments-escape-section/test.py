7416ba463e610c621a524903b19995b817390a9a
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-06-11 23:03:36 +0530

    multiline commit

--- python-for-ai/comments-escape-section/test.py
+++ python-for-ai/comments-escape-section/test.py
@@ -1,3 +1,8 @@
 print("Hello Saurabh \nI am a good boy") #escape sequence 
 #comments are used to explain the code and make it more readable
 #this is a single line comment
+
+
+"""
+Hello saurabh multi line comment
+"""
